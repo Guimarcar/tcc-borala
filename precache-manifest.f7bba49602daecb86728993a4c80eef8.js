1116dc6520607bb501509c44c327883590544f24
Guilherme Marques <guimarcar02@gmail.com>
2020-03-22 20:13:06 -0300

    Updates

---
+++ precache-manifest.f7bba49602daecb86728993a4c80eef8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "17915daf0856d873ada6a773ebf9021f",
+    "url": "/tcc-borala/index.html"
+  },
+  {
+    "revision": "e06d976eb827684244be",
+    "url": "/tcc-borala/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "44c345accb059a08d5d5",
+    "url": "/tcc-borala/static/js/2.db03f008.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/tcc-borala/static/js/2.db03f008.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e06d976eb827684244be",
+    "url": "/tcc-borala/static/js/main.71e6eef6.chunk.js"
+  },
+  {
+    "revision": "bd2366283b968d4c6797",
+    "url": "/tcc-borala/static/js/runtime-main.e978ec88.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/tcc-borala/static/media/logo.5d5d9eef.svg"
+  }
+]);
